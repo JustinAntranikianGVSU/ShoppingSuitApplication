aea9b40c28f18e4c15462402793efaa073874876
Justin Antranikian <justin.antranikian@gmail.com>
2020-03-03 11:17:41 -0700

    added client.cs

---
+++ Domain/Clients/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.Clients
+{
+	public class Client
+	{
+		public Guid Identifier { get; set; }
+
+		public string Name { get; set; }
+
+		public Client(Guid identifier, string name)
+		{
+			Identifier = identifier;
+			Name = name;
+		}
+	}
+}
---
+++ Domain/Clients/ClientLookup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Clients
+{
+	public static class ClientLookup
+	{
+		public static Guid WellFargoClientGuid = new Guid("1560b0b4-3711-4162-9a0e-05caf36ef55a");
+
+		public static Guid BankOfAmericaClientGuid = new Guid("40321985-2a90-41af-958d-606e59bcb19b");
+
+		public static List<Client> Clients;
+
+		static ClientLookup()
+		{
+			Clients = new List<Client>()
+			{
+				new Client(WellFargoClientGuid, "Wells Fargo Inc"),
+				new Client(BankOfAmericaClientGuid, "Bank Of America Global")
+			};
+		}
+
+		public static Client GetClient(Guid identifier)
+		{
+			var client = Clients.SingleOrDefault(oo => oo.Identifier == identifier);
+
+			if (client is null)
+			{
+				throw new Exception($"Could not find {nameof(Client)} with identifier {identifier}. Please fix whatever data is refering to a client that doesn't exist.");
+			}
+
+			return client;
+		}
+	}
+}
